bedc24c6e1e455daf642bc36faf1771bef2496a9
takhangma.rai <takhangma.rai@tai.com.np>
2026-01-22 16:38:03 +0545

    fix: file not found issue

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,4 +5,15 @@ if [ ! -d "vendor" ]; then
   composer install --no-interaction --prefer-dist
 fi
 
+# Install npm dependencies if needed
+if [ ! -d "node_modules" ]; then
+  npm install
+fi
+
+# Build Vite assets AFTER volume is mounted
+npm run build
+
+php artisan key:generate --force
+php artisan migrate --force
+
 php-fpm
